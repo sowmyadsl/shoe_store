b089e193d52d89d1551a9a4542773c4343ef2d96
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-12 15:11:49 -0700

    Add crud functionality and debug the pages to add errors if the name field was left blank

--- app.rb
+++ app.rb
@@ -28,3 +28,56 @@ get('/stores/:id') do
   @brands = Brand.all().-(@store_brands)
   erb(:store)
 end
+
+patch('/stores/:id/edit') do
+  if params.fetch("form_id").==("update_name")
+    @store = Store.find(params.fetch('id').to_i())
+    name = params.fetch('new_name')
+    if @store.update(:name => name)
+      redirect('/stores/'.concat(@store.id().to_s()))
+      erb(:store)
+    else
+      erb(:store_errors)
+    end
+  elsif params.fetch("form_id").==("update_brands")
+    @store = Store.find(params.fetch('id').to_i())
+    new_brand_ids = params[:brand_ids]
+    brand_ids_array = []
+    @store.brands().each() do |brand|
+     brand_ids_array.push(brand.id())
+    end
+    new_brand_ids.each() do |id|
+      brand_ids_array.push(id)
+    end
+    @store.update({:brand_ids => brand_ids_array})
+    redirect('/stores/'.concat(@store.id().to_s()))
+    erb(:store)
+  elsif params.fetch("form_id").==("add_brand")
+    @store = Store.find(params.fetch("id").to_i())
+    name = params.fetch('brand_name')
+    @brand = @store.brands.new({:name => name})
+    @store.brands.push(@brand)
+    redirect('/stores/'.concat(@store.id().to_s()))
+    erb(:store)
+  end
+end
+
+
+delete('/stores/:id') do
+  store_id = params.fetch('id').to_i()
+  @store = Store.find(store_id)
+  @store.destroy()
+  redirect('/stores')
+end
+
+delete('/stores/:id/remove_brand') do
+  store = Store.find(params.fetch("id").to_i)
+  brand_id = params.fetch("brand_id").to_i
+  store.brands.destroy(Brand.find(brand_id))
+  redirect('/stores/'.concat(store.id.to_s))
+end
+
+get('/brands') do
+  @brands = Brand.all()
+  erb(:brands)
+end
