a94a250927c64890f9b1fa6909328a0203707552
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-12 10:22:05 -0700

    Add validation test to check the presence of the brand name and pass it

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,1 +1,12 @@
 require "spec_helper"
+
+
+describe Brand, type: :model do
+
+  it { should have_and_belong_to_many :stores }
+
+  it('validate the presence of name') do
+    test_brand = Brand.new({:name => ""})
+    expect(test_brand.save()).to(eq(false))
+  end
+end
