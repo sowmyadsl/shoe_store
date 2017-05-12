a789bee301c2267a47fe4fffdfeb48073e94e2fb
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-12 10:27:04 -0700

    add test case to capitalize brand name and pass it

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,11 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
    validates(:name, {:presence => true, :length => { :maximum => 20 }})
+   before_save(:capitalize_brand_name)
+
+   private
+
+  define_method(:capitalize_brand_name) do
+    self.name=(name().capitalize())
+  end
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -9,4 +9,14 @@ describe Brand, type: :model do
     test_brand = Brand.new({:name => ""})
     expect(test_brand.save()).to(eq(false))
   end
+
+  it('restricts the length of the name to 20 characters or less') do
+    test_brand = Brand.new({:name => "a".*(21)})
+    expect(test_brand.save()).to(eq(false))
+  end
+
+  it('capitalizes the name') do
+    test_brand = Brand.create(:name => "bata")
+    expect(test_brand.name()).to(eq("Bata"))
+  end
 end
