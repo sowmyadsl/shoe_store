b8bd9d48bfcc0e17bdda1f0e37062642e25a8395
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-16 10:36:25 -0700

    Add integration spec for brands page and pass them

--- spec/brand_pages_spec.rb
+++ spec/brand_pages_spec.rb
@@ -28,3 +28,24 @@ describe('see brand details path', {:type => :feature}) do
     expect(page).to have_content("Current Brand Name: Puma")
   end
 end
+
+describe('update brand path', {:type => :feature}) do
+  it('allows a user to rename the store') do
+    test_brand = Brand.create({:name => "adidas", :price => 100})
+  visit('/brands')
+    click_link('Adidas')
+    fill_in('new_name', :with => "puma")
+    click_button('Update')
+    expect(page).to have_content("Puma")
+  end
+end
+
+describe('delete brand path', {:type => :feature}) do
+  it('allows a user to delete a brand from the database') do
+    test_brand = Brand.create({:name => "adidas",:price => 150})
+    visit('/brands')
+    click_link('Adidas')
+    click_button('Delete')
+    expect(page).to have_no_content('Adidas')
+  end
+end
