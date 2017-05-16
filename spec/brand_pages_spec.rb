b2544a221895fc9ce1711e20a6e19400bd36320b
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-16 10:32:26 -0700

    Add integration spec for brands and pass them

--- spec/brand_pages_spec.rb
+++ spec/brand_pages_spec.rb
@@ -10,3 +10,21 @@ describe('add a brand to a store route', {:type => :feature}) do
     expect(page).to have_content("Puma")
   end
 end
+
+describe('list all brands route', {:type => :feature}) do
+  it('allows a user to see all of the brands in the database') do
+    test_store = Brand.create({:name => "puma"})
+    test_store2 = Brand.create({:name => "adidas"})
+    visit('/brands')
+    expect(page).to have_content(test_store.name)
+  end
+end
+
+describe('see brand details path', {:type => :feature}) do
+  it('allows a user to view the brands details') do
+    test_brand = Brand.create({:name => "puma", :price => 50})
+    visit('/brands')
+    click_link('Puma')
+    expect(page).to have_content("Current Brand Name: Puma")
+  end
+end
